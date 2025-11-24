8af940ad873960f78bf8760d1df3ce7a2c2c9879
f0rte <y4859.rikoo_phy-sur21@outlook.jp>
2025-11-25 00:08:55 +0900

    feat: setup sample contents

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from "astro:content";
+
+const eventsCollection = defineCollection({
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        date: z.date(),
+        description: z.string(),
+        tags: z.array(z.string()).optional(),
+    }),
+});
+
+export const collections = {
+    "events": eventsCollection,
+};
---
+++ src/content/events/sample.md
@@ -0,0 +1,11 @@
+---
+title: "ポートフォリオを作成する(Sample contents)"
+date: 2025-11-24
+description: "Astroでポートフォリオを作成中"
+tags: ["Astro", "AWS"]
+---
+
+## 概要
+ポートフォリオを作ってなかったので、超シンプルなポートフォリオを作りたいとGeminiに聞き、Astroを採用しました  
+初めてAstroを使いました  
+AWSのAmplifyにデプロイしようと思います
